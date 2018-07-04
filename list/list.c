b3b0acdca27406b296d948b0167c0b2857c635d6
sarait <saraithernandez0205@gmail.com>
2018-07-04 17:12:39 -0400

    problemas con node *next

--- list/list.c
+++ list/list.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
 	int data;
-	node next;
+	struct node *next;
 }node;
 
 typedef struct{
     node *head;
-    int size = 0;
+    node *end;
+    int size;
 }list;
 
 int list_size(list* l);
@@ -21,8 +23,9 @@ void list_clear(list* l); // limpia la lista
 
 int main()
 {
-
-
+	list *lista;
+	lista = malloc(sizeof(lista));
+	printf("lista size: %d\n",lista->size);
 
 	return 0;
 }
@@ -39,14 +42,7 @@ int list_front(list* l)
 
 int list_back(list* l)
 {
-	int i;
-	node *aux;
-	aux = l->head;
-	
-	for(i = 0; i < l->size; i++)
-		aux = aux->next;
-
-	return aux->data;
+	return l->end->data;
 }
 
 int list_pop_front(list* l)
@@ -57,11 +53,12 @@ int list_pop_front(list* l)
 	if(l->size >1)
 		l->head = l->head->next;
 	else if(l->size == 1)
-		l->head = NULL;
+		l->head = NULL, l->end = NULL;
 	else
 		return -666;
 
 	(l->size)--;
+	//free(aux);
 	return aux->data;
 }
 
@@ -70,44 +67,79 @@ int list_pop_back(list* l)
 	int i;
 	node *aux, *aux2;
 	aux = l->head;
+	aux2 = l->end;
 
 	if(l->size > 1)
 	{
 		for (i = 0; i < l->size-1; i++)
-			aux2 = aux2->next;
-		aux = aux2->next;
-		aux2->next = NULL;
+			aux = aux->next;
+		l->end = aux;
+		aux->next = NULL;
 	}else if (l->size == 1)
 	{
 		l->head = NULL;
+		l->end = NULL;
 	}else
 		return -666;
 
 	(l->size)--;
-	return aux->data;
+	return aux2->data;
 }
 
 void list_add_front(list* l, int data)
 {
 	node *new;
-	new->data = data;
-	if(l->size > 0)
-		new->next = l->head;
+	if((new = malloc(sizeof(node))) == NULL)
+		printf("error malloc\n");
 	else
-		new->next = NULL;
-	l->head = new;
-	(l->size)++;
+	{
+		new->data = data;
+
+		if(l->size > 0)
+			new->next = l->head;
+		else
+		{
+			l->end = new;
+			new->next = NULL;
+		}
+
+		l->head = new;
+		(l->size)++;
+	}
 }
 
 void list_add_back(list* l, int data)
 {
 	node *new;
-	new->data = data;
-	new->next = l->head;
-	l->head = new;
+	if((new = malloc(sizeof(node))) == NULL)
+		printf("error malloc\n");
+	else
+	{
+		new->data = data;
+		
+		if(l->size > 0)
+			l->end->next = new;
+		else
+			l->head = new;
+
+		new->next=NULL;
+		l->end = new;
+		(l->size)++;
+	}
 }
 
 void list_clear(list* l)
 {
-
+	int i;
+	node *aux;
+	if (l->size > 0)
+	{
+		for(i = 0; i< l->size; i++)
+		{
+			aux = l->head;
+			l->head = l->head->next;
+			free(aux);
+		}	
+	}
+	l->size = 0;
 }
